2c94034986b815766bb8c841dcd82b3dbd765a49
Harish Anand Vijayakumar <hvijayakumar@wisc.edu>
2023-09-14 15:57:03 -0500

    Update HW01/task6.cpp

--- HW01/task6.cpp
+++ HW01/task6.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
-int main()
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc , char *argv[])
 {
-    int N;
+    int N = std::atoi(argv[1]);
     std::cout<<"Getting N -command line argument from user: ";
-    std::cin>>N;
+
     std::cout<<"\nPrinting the numbers in ascending from 0 to N \n";
 
     for(int i=0;i<=N;i++)
       {
         printf("%d ",i);
       }
-    
+
     std::cout<<"\nPrinting the numbers in descending from N to 0 \n";
-    
+
     for(int j=N;j>=0;j--)
       {
-       std::cout<<j<<" ";
+        std::cout<<j<<" ";
       }
-    
+
     return 0;
 }
 
