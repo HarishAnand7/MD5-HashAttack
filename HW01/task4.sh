8da4da1f28f9b8844ca65e439f26935fd10e43f6
Harish Anand Vijayakumar <hvijayakumar@wisc.edu>
2023-09-14 15:19:52 -0500

    Update HW01/task4.sh

--- HW01/task4.sh
+++ HW01/task4.sh
@@ -3,10 +3,12 @@
 #SBATCH --partition=instruction
 #SBATCH --ntasks=1 --cpus-per-task=2
 #SBATCH --time=0-00:00:10
-#SBATCH --output=FirstSlurm-%j.out
-#SBATCH --output=FirstSlurm-%j.err
+#SBATCH --output=FirstSlum-%j.out
+#SBATCH --error=FirstSlurm-%j.err
 
 
 ##cd $SLURM_SUBMIT_DIR
 
+echo "Hostname: "
 hostname
+~
