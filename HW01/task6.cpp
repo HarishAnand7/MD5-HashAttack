ab3e0ab7ef439c86c63c9a9de281c921b8dce1b7
Harish Anand Vijayakumar <hvijayakumar@wisc.edu>
2023-09-14 15:10:10 -0500

    Update HW01/task6.cpp

--- HW01/task6.cpp
+++ HW01/task6.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+int main()
+{
+    int N;
+    std::cout<<"Getting N -command line argument from user: ";
+    std::cin>>N;
+    std::cout<<"\nPrinting the numbers in ascending from 0 to N \n";
+
+    for(int i=0;i<=N;i++)
+      {
+        printf("%d ",i);
+      }
+    
+    std::cout<<"\nPrinting the numbers in descending from N to 0 \n";
+    
+    for(int j=N;j>=0;j--)
+      {
+       std::cout<<j<<" ";
+      }
+    
+    return 0;
+}
 
