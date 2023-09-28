b98fc367ebb9f33abec584fb4c6cf23612fc77f8
Harish Anand Vijayakumar <hvijayakumar@wisc.edu>
2023-09-28 13:42:12 -0500

    Add new file

---
+++ HW03/task1.cu
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <cuda_runtime.h>
+
+__global__
+void factorial()
+{
+    int fact = 1;
+
+    int index =threadIdx.x+1;
+
+    for (int i = 1; i < index+1; i++)
+    {
+        fact *= i;
+    }
+
+    std::printf("%d!=%d \n", index, fact);
+}
+
+int main()
+{
+    factorial<<<1, 8>>>();
+
+    cudaDeviceSynchronize();
+
+    return 0;
+}
