bc2fc678ac68d8af2c49de9a83624b2d7f75dfef
Harish Anand Vijayakumar <hvijayakumar@wisc.edu>
2023-10-25 12:56:32 -0500

    Update task1_thrust.sh

--- HW07/task1_thrust.sh
+++ HW07/task1_thrust.sh
@@ -12,7 +12,7 @@ cd $SLURM_SUBMIT_DIR
 
 nvcc task1_thrust.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o task1_thrust
 
-for i in {10..55};
+for i in {10..20};
 do
    ./task1_thrust $((2**i))
 done
