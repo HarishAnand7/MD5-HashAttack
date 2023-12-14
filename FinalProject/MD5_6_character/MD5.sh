bd0cc9f0e3154eb53ee78bb107856df93d1ad647
Harish Anand Vijayakumar <hvijayakumar@wisc.edu>
2023-12-13 19:46:54 -0600

    Add new file

---
+++ FinalProject/MD5_6_character/MD5.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+#SBATCH --job-name=task
+#SBATCH --partition=instruction
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-02:00:00
+#SBATCH --gres=gpu:2
+#SBATCH --error=errorq1-%j.txt
+#SBATCH --output=outputq1-%j.txt
+
+
+cd $SLURM_SUBMIT_DIR
+
+
+nvcc  -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o 
+./
+
